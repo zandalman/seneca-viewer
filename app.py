c2fc30ab4b59c8c3cd3ae300e0445fe825a73374
Laura <laura.zhou@yale.edu>
2020-08-14 17:46:43 -0400

    Updated configuration file, added tab content

--- app.py
+++ app.py
@@ -10,19 +10,39 @@ app.config['SIJAX_STATIC_PATH'] = sijax_path
 app.config['SIJAX_JSON_URI'] = '/static/js/sijax/json2.js'
 flask_sijax.Sijax(app)
 
-
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
+class SijaxHandlers(object):
+    def get_json(obj_response, file):
+        '''
+        Updates #jsonDisplay to contain the selected JSON input, if valid.
+
+                Parameters:
+                        file (string): File name
+        '''
+        with open(os.path.join(logic_path, file), "r") as json_file:
+            try:
+                textCallback = json.dumps(json.load(json_file), indent=2)
+                obj_response.script('$("#jsonDisplay").attr("validity","True")')
+            except ValueError as err:
+                textCallback = "file is invalid JSON"
+                obj_response.script('$("#jsonDisplay").attr("validity","False")')
+            json_file.close()
+        obj_response.html("#jsonDisplay", textCallback)
+        json_file.close()
 
 instance_path = os.path.dirname(pathlib.Path(app.instance_path))
-logic_path = os.path.join(instance_path, "json_files")
-
+json_files_path = os.path.join(instance_path, "json_files")
 
 @app.route("/spreadsheet", methods=("GET", "POST"))
 def spreadsheet():
-    savedFiles = [f for f in os.listdir(logic_path) if os.path.isfile(os.path.join(logic_path, f))]
+    if g.sijax.is_sijax_request:
+        g.sijax.register_callback("get_json", SijaxHandlers.get_json)
+        return g.sijax.process_request()
+
+    savedFiles = [f for f in os.listdir(json_files_path) if os.path.isfile(os.path.join(json_files_path, f))]
 
     with open(os.path.join(instance_path, "config.txt"), "r") as config_file:
         config_json = json.load(config_file)
@@ -68,38 +88,20 @@ def spreadsheet():
             json.dump(logic, file, indent=2)
     return render_template("spreadsheet.html",
                            files=savedFiles,
+                           file_folder = json_files_path,
                            configfile=os.path.join(instance_path, "config.txt"),
                            config=json.dumps(event_config))
 
 
 @app.route('/timeline', methods=['GET', 'POST'])
 def timeline():
-    files = [f for f in os.listdir(logic_path) if os.path.isfile(os.path.join(logic_path, f))]
-
-    def get_json(obj_response, file):
-        '''
-        Updates #jsonDisplay to contain the selected JSON input, if valid.
-
-                Parameters:
-                        file (string): File name
-        '''
-        with open(os.path.join(logic_path, file), "r") as json_file:
-            try:
-                textCallback = json.dumps(json.load(json_file), indent=2)
-                obj_response.script('$("#jsonDisplay").attr("validity","True")')
-            except ValueError as err:
-                textCallback = "file is invalid JSON"
-                obj_response.script('$("#jsonDisplay").attr("validity","False")')
-            json_file.close()
-        obj_response.html("#jsonDisplay", textCallback)
-        json_file.close()
-
-    g.sijax.register_callback('get_json', get_json)
+    savedFiles = [f for f in os.listdir(json_files_path) if os.path.isfile(os.path.join(logic_path, f))]
 
     if g.sijax.is_sijax_request:
+        g.sijax.register_callback('get_json', get_json)
         return g.sijax.process_request()
 
-    return render_template('timeline.html', json_options=files, file_folder=logic_path)
+    return render_template('timeline.html', json_options=savedFiles, file_folder=json_files_path)
 
 
 if __name__ == '__main__':
