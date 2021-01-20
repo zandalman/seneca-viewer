15d071470c464369a78a064f06b00229c03ef1ee
Zachary Andalman <zandalman@gmail.com>
2021-01-20 16:02:37 -0600

    Multiple tables

--- static/js/main.js
+++ static/js/main.js
@@ -1,13 +1,24 @@
 $( document ).ready(function() {
     // Initialize tabs
     $("#tabs").tabs();
+    // Create tables
+    experimentTable = createExperimentTable();
+    createEventTables();
+    addEventTableHooks();
+    // Initialize select box
+    $("#event-type").select2({
+        "allowClear": true,
+        "placeholder": "Event Type Filter"
+    });
 });
+
+var eventList = [];
+var eventTables = [];
 
 // Define regex expressions
 var NUM_REGEX = /^-?(0|[1-9]\d*)?(\.\d+)?(?<=\d)(e-?(0|[1-9]\d*))?([yzafpnumkMGTPEZY]$|$)/;
 var VAR_REGEX = /(?=^[a-zA-Z])(?=^[a-zA-Z0-9\-_]+$)(?=^(?!(true|false)$))/;
 var BOOL_REGEX = /^(true|false|)$/;
-
 var MU = "\u03BC";
 
 // Define unit conversions
@@ -31,9 +42,80 @@ var UNITS = {
     "Y": 1e24
 };
 
-Object.keys(UNITS).forEach(function(key) {
-    $(".unit-select").append("<option value='" + key + "'>" + key + "</option>");
-});
+var eventTypeDataJSON = JSON.parse($("#storage").data("evtyp").replaceAll("'", "\""));
+
+var createEventTables = function () {
+    Object.keys(eventTypeDataJSON).forEach(function (eventType) {
+        var eventTypeData = eventTypeDataJSON[eventType];
+        $("#event-type").append("<option value='" + eventType + "'>" + eventType + "</option>");
+        $("#event-tables").append("<p>" + eventType + "</p>");
+        $("#event-tables").append("<div class='table' id='table-" + eventType + "'></div>");
+        eventTables.push(createEventTable(eventType, eventTypeData));
+    });
+}
+
+var addEventTableHooks = function () {
+    eventTables.forEach(function (eventTable) {
+        addBeforeRemoveRowHook(eventTable);
+        addAfterRemoveRowHook(eventTable);
+        addAfterChangeHook(eventTable);
+    });
+}
+
+var createEventTable = function (eventType, eventTypeData) {
+    var numParams = Object.keys(eventTypeData.params).length;
+    var sortedParamNames = Object.keys(eventTypeData.params).sort();
+    var defaultTableData = [Array.apply(null, Array(numParams + 1)).map(String.prototype.valueOf, "")];
+    defaultTableData[0][0] = eventType + "_event";
+    eventList.push(eventType + "_event");
+    // Define table
+    var container = document.getElementById("table-" + eventType);
+    var eventTable = new Handsontable(container, {
+        data: defaultTableData,
+        fixedColumnsLeft: 1,
+        manualRowMove: true,
+        contextMenu: {
+            callback: function (key, selection, clickEvent) {
+                console.log(key, selection, clickEvent);
+            },
+            items: {
+                "row_above": {
+                    name: "Add event above"
+                },
+                "row_below": {
+                    name: "Add event below"
+                },
+                "remove_row": {
+                    name: "Remove event"
+                },
+                "copy": {},
+                "cut": {},
+                "undo": {},
+                "redo": {}
+            }
+        },
+        colHeaders: ["events"].concat(sortedParamNames),
+        rowHeaders: true,
+        cells: function(row, column, prop) {
+            var cellProperties = {};
+            var visualRowIndex = this.instance.toVisualRow(row);
+            var visualColIndex = this.instance.toVisualColumn(column);
+            if (visualColIndex === 0) {
+                cellProperties.renderer = firstColRenderer;
+            } else {
+                var paramType = eventTypeData.params[sortedParamNames[visualColIndex - 1]].type;
+                cellProperties.validator = generateParamValidator(paramType);
+                cellProperties.renderer = generateParamRenderer(paramType);
+                cellProperties.strict = true;
+                cellProperties.allowInvalid = false;
+            }
+            return cellProperties;
+        },
+        preventOverflow: "horizontal",
+        licenseKey: "non-commercial-and-evaluation"
+    });
+    return eventTable;
+}
 
 var code_mirror = CodeMirror(document.getElementById("code-editor"), {
   value: "x = 2\n",
@@ -56,10 +138,6 @@ function firstColRenderer(instance, td, row, col, prop, value, cellProperties) {
     td.style.fontWeight = "bold";
     td.style.background = "#EEE";
 }
-
-$("#events").on("input", ".colorpicker", function () {
-    experimentTable.render();
-});
 
 $("#add-col").on("click", function () {
     experimentTable.alter("insert_col", experimentTable.getData()[0].length, $("#num-col").val());
@@ -151,191 +229,139 @@ var generateParamRenderer = function (paramType) {
     return paramRenderer;
 };
 
-$("#variables").on("mouseenter, mouseleave", "li", function () {
-    sineEventTable.render();
-});
-
-var tableDataDefault2 = [
-    ["ev1", "", "", ""],
-    ["ev2", "", "", ""],
-    ["ev3", "", "", ""],
-    ["ev4", "", "", ""],
-    ["ev5", "", "", ""],
-    ["ev6", "", "", ""],
-    ["ev7", "", "", ""]
-];
-var exampleParamTypes = ["float", "int", "boolean"]
-
-// Define table
-var container2 = document.getElementById("sine-table");
-var sineEventTable = new Handsontable(container2, {
-    data: tableDataDefault2,
-    fixedColumnsLeft: 1,
-    manualRowMove: true,
-    contextMenu: {
-        callback: function (key, selection, clickEvent) {
-            console.log(key, selection, clickEvent);
+var createExperimentTable = function () {
+    var container = document.getElementById("exp-table");
+    var experimentTable = new Handsontable(container, {
+        data: tableDataDefault,
+        fixedColumnsLeft: 1,
+        manualRowMove: true,
+        contextMenu: {
+            callback: function (key, selection, clickEvent) {
+                console.log(key, selection, clickEvent);
+            },
+            items: {
+                "col_left": {
+                    name: "Add timestep left",
+                    disabled: function () {
+                        return this.getSelectedLast()[1] === 0;
+                    }
+                },
+                "col_right": {
+                    name: "Add timestep right"
+                },
+                "remove_col": {
+                    name: "Remove timestep",
+                    disabled: function () {
+                        return this.getSelectedLast()[1] === 0;
+                    }
+                },
+                "copy": {},
+                "cut": {},
+                "undo": {},
+                "redo": {}
+            }
         },
-        items: {
-            "row_above": {
-                name: "Add event above"
-            },
-            "row_below": {
-                name: "Add event below"
-            },
-            "remove_row": {
-                name: "Remove event"
-            },
-            "copy": {},
-            "cut": {},
-            "undo": {},
-            "redo": {}
-        }
-    },
-    colHeaders: ["events", "float", "int", "bool"],
-    rowHeaders: true,
-    cells: function(row, column, prop) {
-        var cellProperties = {};
-        var visualRowIndex = this.instance.toVisualRow(row);
-        var visualColIndex = this.instance.toVisualColumn(column);
-        if (visualColIndex === 0) {
-            cellProperties.renderer = firstColRenderer;
-        } else {
-            var paramType = exampleParamTypes[visualColIndex - 1];
-            cellProperties.validator = generateParamValidator(paramType);
-            cellProperties.renderer = generateParamRenderer(paramType);
-            cellProperties.strict = true;
-            cellProperties.allowInvalid = false;
-        }
-        return cellProperties;
-    },
-    preventOverflow: "horizontal",
-    licenseKey: "non-commercial-and-evaluation"
-});
-
-
-// Define table
-var container = document.getElementById("exp-table");
-var experimentTable = new Handsontable(container, {
-    data: tableDataDefault,
-    fixedColumnsLeft: 1,
-    manualRowMove: true,
-    contextMenu: {
-        callback: function (key, selection, clickEvent) {
-            console.log(key, selection, clickEvent);
+        colHeaders: function(index) {
+            return index > 0 ? index : "channels";
         },
-        items: {
-            "col_left": {
-                name: "Add timestep left",
-                disabled: function () {
-                    return this.getSelectedLast()[1] === 0;
-                }
-            },
-            "col_right": {
-                name: "Add timestep right"
-            },
-            "remove_col": {
-                name: "Remove timestep",
-                disabled: function () {
-                    return this.getSelectedLast()[1] === 0;
-                }
-            },
-            "copy": {},
-            "cut": {},
-            "undo": {},
-            "redo": {}
-        }
-    },
-    colHeaders: function(index) {
-        return index > 0 ? index : "channels";
-    },
-    rowHeaders: true,
-    cells: function(row, column, prop) {
-        const cellProperties = {};
-        const visualRowIndex = this.instance.toVisualRow(row);
-        const visualColIndex = this.instance.toVisualColumn(column);
-        if (visualColIndex === 0) {
-            cellProperties.editor = "text";
-            cellProperties.renderer = firstColRenderer;
-        } else {
-            cellProperties.type = "autocomplete";
-            cellProperties.source = sineEventTable.getDataAtCol(0).filter(function (eventName) {return !(eventName === null || eventName === "");});
-            cellProperties.strict = true;
-            cellProperties.allowInvalid = false;
-        }
-        return cellProperties;
-    },
-    preventOverflow: "horizontal",
-    licenseKey: "non-commercial-and-evaluation"
-});
+        rowHeaders: true,
+        cells: function(row, column, prop) {
+            const cellProperties = {};
+            const visualRowIndex = this.instance.toVisualRow(row);
+            const visualColIndex = this.instance.toVisualColumn(column);
+            if (visualColIndex === 0) {
+                cellProperties.editor = "text";
+                cellProperties.renderer = firstColRenderer;
+            } else {
+                cellProperties.type = "autocomplete";
+                cellProperties.source = eventList;
+                cellProperties.strict = true;
+                cellProperties.allowInvalid = false;
+            }
+            return cellProperties;
+        },
+        preventOverflow: "horizontal",
+        licenseKey: "non-commercial-and-evaluation"
+    });
+    return experimentTable;
+}
 
-// Clear cells in experiment table when removing events in event tables
-sineEventTable.addHook("beforeRemoveRow", function (index, amount, physicalRows, source) {
-    var expData = experimentTable.getData();
-    var eventNames = sineEventTable.getDataAtCol(0);
-    var removedEventNames = physicalRows.map(function (rowIndex) {return eventNames[rowIndex];});
-    var expChanges = [];
-    for (i = 0; i < expData.length; i++) {
-        for (j = 0; j < expData[i].length; j++){
-            if (removedEventNames.includes(expData[i][j])) {
-                expChanges.push([i, j, ""]);
+var addBeforeRemoveRowHook = function (eventTable) {
+    eventTable.addHook("beforeRemoveRow", function (index, amount, physicalRows, source) {
+        var expData = experimentTable.getData();
+        var eventNames = eventTable.getDataAtCol(0);
+        var removedEventNames = physicalRows.map(function (rowIndex) {return eventNames[rowIndex];});
+        var expChanges = [];
+        for (i = 0; i < expData.length; i++) {
+            for (j = 0; j < expData[i].length; j++){
+                if (removedEventNames.includes(expData[i][j])) {
+                    expChanges.push([i, j, ""]);
+                }
             }
         }
-    }
-    if (expChanges.length > 0) {
-        confirmMessage = "Are you sure you want to delete "
-            + (removedEventNames.length > 1 ? String(removedEventNames.length) + " events? " : "event '" + removedEventNames[0] + "'? ")
-            + String(expChanges.length) + " cells will be cleared.";
-        if (confirm(confirmMessage)){
-            experimentTable.setDataAtCell(expChanges);
-            return true;
-        } else {
-            return false;
+        if (expChanges.length > 0) {
+            confirmMessage = "Are you sure you want to delete "
+                + (removedEventNames.length > 1 ? String(removedEventNames.length) + " events? " : "event '" + removedEventNames[0] + "'? ")
+                + String(expChanges.length) + " cells will be cleared.";
+            if (confirm(confirmMessage)){
+                experimentTable.setDataAtCell(expChanges);
+            } else {
+                return false;
+            }
         }
-    } else {
+        removedEventNames.forEach(function (eventName) {
+            eventIndex = eventList.indexOf(eventName);
+            if (eventIndex !== -1) {
+                eventList.splice(eventIndex, 1);
+            }
+        });
         return true;
-    }
-});
+    });
+}
 
-sineEventTable.addHook("afterRemoveRow", function (index, amount, physicalRows, source) {
-    var currentVariables = $("#variables li").map(function() {
-        return $(this).data("name");
-    }).get();
-    currentVariables.map(function(variableName) {
-        if (!(sineEventTable.getData().flat().includes(variableName))) {
-            $("#variables li[data-name='" + variableName + "']").remove();
-        }
+var addAfterRemoveRowHook = function (eventTable) {
+    eventTable.addHook("afterRemoveRow", function (index, amount, physicalRows, source) {
+        var currentVariables = $("#variables li").map(function () {
+            return $(this).data("name");
+        }).get();
+        currentVariables.map(function (variableName) {
+            if (!(eventTable.getData().flat().includes(variableName))) {
+                $("#variables li[data-name='" + variableName + "']").remove();
+            }
+        });
     });
-});
+}
 
-sineEventTable.addHook("afterChange", function(changes, source) {
-    if (source === "loadData") {
-      return; //don't save this change
-    }
-    var currentVariables = $("#variables li").map(function() {
-        return $(this).data("name");
-    }).get();
-    changes.forEach(function(change) {
-        var col = change[1];
-        var value = change[3];
-        if (value.match(VAR_REGEX) && col !== 0 && !currentVariables.includes(value)) {
-            $("#variables").append("<li data-name='" + value + "'>" + value + "<li>");
-            // Sort variables alphabetically
-            $("#variables li").sort(function(a, b) {
-                return ($(a).text().toUpperCase()).localeCompare($(b).text().toUpperCase());
-            }).appendTo("#variables");
-            // Remove empty list items from varibale list
-            $("#variables li").filter(function () {
-               return $(this).text() === "";
-            }).remove();
+var addAfterChangeHook = function (eventTable) {
+    eventTable.addHook("afterChange", function(changes, source) {
+        if (source === "loadData") {
+          return; //don't save this change
         }
+        var currentVariables = $("#variables li").map(function() {
+            return $(this).data("name");
+        }).get();
+        changes.forEach(function(change) {
+            var col = change[1];
+            var value = change[3];
+            if (value.match(VAR_REGEX) && col !== 0 && !currentVariables.includes(value)) {
+                $("#variables").append("<li data-name='" + value + "'>" + value + "<li>");
+                // Sort variables alphabetically
+                $("#variables li").sort(function(a, b) {
+                    return ($(a).text().toUpperCase()).localeCompare($(b).text().toUpperCase());
+                }).appendTo("#variables");
+                // Remove empty list items from varibale list
+                $("#variables li").filter(function () {
+                   return $(this).text() === "";
+                }).remove();
+            }
+        });
+        currentVariables.map(function(variableName) {
+            if (!(eventTable.getData().flat().includes(variableName))) {
+                $("#variables li[data-name='" + variableName + "']").remove();
+            }
+        });
     });
-    currentVariables.map(function(variableName) {
-        if (!(sineEventTable.getData().flat().includes(variableName))) {
-            $("#variables li[data-name='" + variableName + "']").remove();
-        }
-    });
-});
+}
 
 function loadJson(loadedData) {
     var loadData = JSON.parse(loadedData).data;
