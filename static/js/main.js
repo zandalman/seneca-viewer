54a4b443bbc518c226b1df3294f6aa268cf861e0
Zachary Andalman <zandalman@gmail.com>
2021-02-19 11:08:10 -0500

    Thinking about adding a custom editor in experiment table with a menu of event types and a submenu of specific events

--- static/js/main.js
+++ static/js/main.js
@@ -334,7 +334,7 @@ var createEventTable = function (eventType, eventTypeData, eventTableData) {
             var cellProperties = {};
             if (column === 0) {
                 cellProperties.readOnly = true;
-                cellProperties.renderer = headerRenderer;
+                cellProperties.renderer = eventTableHeaderRenderer;
             } else {
                 var paramType = eventTypeData.params[sortedParamNames[column - 1]].type;
                 cellProperties.className = ["cell-" + paramType];
@@ -357,14 +357,179 @@ var createEventTable = function (eventType, eventTypeData, eventTableData) {
 }
 
 // Define renderer to disguise table cells as table headers
-function headerRenderer(instance, td, row, col, prop, value, cellProperties) {
+function eventTableHeaderRenderer(instance, td, row, col, prop, value, cellProperties) {
     Handsontable.renderers.TextRenderer.apply(this, arguments);
-    if (selectedVariable && instance.getDataAtRow(row).includes(selectedVariable)) {
+    if (selectedVariable && Object.keys(getEventVariables(value)).includes(selectedVariable)) {
         td.style.backgroundColor = COLORS.highlight;
     } else {
         td.style.backgroundColor = COLORS.header;
     }
     td.style.color = "black";
+}
+
+// Define renderer to disguise table cells as table headers
+function experimentTableHeaderRenderer(instance, td, row, col, prop, value, cellProperties) {
+    Handsontable.renderers.TextRenderer.apply(this, arguments);
+    var rowData = instance.getDataAtRow(row);
+    if (selectedVariable && Object.keys(getChannelVariables(rowData)).includes(selectedVariable)) {
+        td.style.backgroundColor = COLORS.highlight;
+    } else {
+        td.style.backgroundColor = COLORS.header;
+    }
+    td.style.color = "black";
+}
+
+class selectEditor extends Handsontable.editors.BaseEditor {
+    // Initialize editor instance
+    init() {
+        // Create detached node, add CSS class and make sure its not visible
+        this.select = this.hot.rootDocument.createElement("SELECT");
+        Handsontable.dom.addClass(this.select, "htSelectEditor");
+        this.select.style.display = "none";
+        // Attach node to DOM by appending it to the container holding the table
+        this.hot.rootElement.appendChild(this.select);
+    }
+    // Create options
+    prepare(row, col, prop, td, originalValue, cellProperties) {
+        super.prepare(row, col, prop, td, originalValue, cellProperties);
+        const selectOptions = this.cellProperties.selectOptions;
+        let options;
+        if (typeof selectOptions === 'function') {
+            options = this.prepareOptions(selectOptions(this.row, this.col, this.prop));
+            } else {
+            options = this.prepareOptions(selectOptions);
+        }
+        Handsontable.dom.empty(this.select);
+        Handsontable.helper.objectEach(options, (value, key) => {
+            const optionElement = this.hot.rootDocument.createElement('OPTION');
+            optionElement.value = key;
+            Handsontable.dom.fastInnerHTML(optionElement, value);
+            this.select.appendChild(optionElement);
+        });
+    }
+    prepareOptions(optionsToPrepare) {
+        let preparedOptions = {};
+        if (Array.isArray(optionsToPrepare)) {
+            for (let i = 0, len = optionsToPrepare.length; i < len; i++) {
+                preparedOptions[optionsToPrepare[i]]=optionsToPrepare[i];
+            }
+        } else if (typeof optionsToPrepare==='object' ) {
+            preparedOptions=optionsToPrepare;
+        }
+        return preparedOptions;
+    }
+    getValue() {
+        return this.select.value;
+    }
+    setValue(value) {
+        this.select.value = value;
+    }
+    open() {
+        this._opened = true;
+        this.refreshDimensions();
+        this.select.style.display = '';
+    }
+    refreshDimensions() {
+        this.TD = this.getEditedCell();
+        // TD is outside of the viewport.
+        if (!this.TD) {
+            this.close();
+            return;
+        }
+        const { wtOverlays } = this.hot.view.wt;
+        const currentOffset = Handsontable.dom.offset(this.TD);
+        const containerOffset = Handsontable.dom.offset(this.hot.rootElement);
+        const scrollableContainer = wtOverlays.scrollableElement;
+        const editorSection = this.checkEditorSection();
+        let width = Handsontable.dom.outerWidth(this.TD) + 1;
+        let height = Handsontable.dom.outerHeight(this.TD) + 1;
+        let editTop = currentOffset.top - containerOffset.top - 1 - (scrollableContainer.scrollTop || 0);
+        let editLeft = currentOffset.left - containerOffset.left - 1 - (scrollableContainer.scrollLeft || 0);
+        let cssTransformOffset;
+        switch (editorSection) {
+            case 'top':
+                cssTransformOffset = Handsontable.dom.getCssTransform(wtOverlays.topOverlay.clone.wtTable.holder.parentNode);
+                break;
+            case 'left':
+                cssTransformOffset = Handsontable.dom.getCssTransform(wtOverlays.leftOverlay.clone.wtTable.holder.parentNode);
+                break;
+            case 'top-left-corner':
+                cssTransformOffset = Handsontable.dom.getCssTransform(wtOverlays.topLeftCornerOverlay.clone.wtTable.holder.parentNode);
+                break;
+            case 'bottom-left-corner':
+                cssTransformOffset = Handsontable.dom.getCssTransform(wtOverlays.bottomLeftCornerOverlay.clone.wtTable.holder.parentNode);
+                break;
+            case 'bottom':
+                cssTransformOffset = Handsontable.dom.getCssTransform(wtOverlays.bottomOverlay.clone.wtTable.holder.parentNode);
+                break;
+            default:
+                break;
+        }
+        if (this.hot.getSelectedLast()[0] === 0) {
+            editTop += 1;
+        }
+        if (this.hot.getSelectedLast()[1] === 0) {
+            editLeft += 1;
+        }
+        const selectStyle = this.select.style;
+        if (cssTransformOffset && cssTransformOffset !== -1) {
+            selectStyle[cssTransformOffset[0]] = cssTransformOffset[1];
+        } else {
+            Handsontable.dom.resetCssTransform(this.select);
+        }
+        const cellComputedStyle = Handsontable.dom.getComputedStyle(this.TD, this.hot.rootWindow);
+        if (parseInt(cellComputedStyle.borderTopWidth, 10) > 0) {
+            height -= 1;
+        }
+        if (parseInt(cellComputedStyle.borderLeftWidth, 10) > 0) {
+            width -= 1;
+        }
+        selectStyle.height = `${height}px`;
+        selectStyle.minWidth = `${width}px`;
+        selectStyle.top = `${editTop}px`;
+        selectStyle.left = `${editLeft}px`;
+        selectStyle.margin = '0px';
+    }
+    getEditedCell() {
+        const { wtOverlays } = this.hot.view.wt;
+        const editorSection = this.checkEditorSection();
+        let editedCell;
+        switch (editorSection) {
+        case 'top':
+            editedCell = wtOverlays.topOverlay.clone.wtTable.getCell({
+                row: this.row,
+                col: this.col
+            });
+            this.select.style.zIndex = 101;
+            break;
+        case 'corner':
+            editedCell = wtOverlays.topLeftCornerOverlay.clone.wtTable.getCell({
+                row: this.row,
+                col: this.col
+            });
+            this.select.style.zIndex = 103;
+            break;
+        case 'left':
+            editedCell = wtOverlays.leftOverlay.clone.wtTable.getCell({
+                row: this.row,
+                col: this.col
+            });
+            this.select.style.zIndex = 102;
+            break;
+        default:
+            editedCell = this.hot.getCell(this.row, this.col);
+            this.select.style.zIndex = '';
+            break;
+        }
+        return editedCell < 0 ? void 0 : editedCell;
+    }
+    focus() {
+        this.select.focus();
+    }
+    close() {
+        this._opened = false;
+        this.select.style.display = 'none';
+    }
 }
 
 // Define renderer for experiment table
@@ -581,7 +746,7 @@ var createExperimentTable = function (experimentTableData = null) {
             const cellProperties = {};
             if (column < 2) {
                 cellProperties.readOnly = true;
-                cellProperties.renderer = headerRenderer;
+                cellProperties.renderer = experimentTableHeaderRenderer;
             } else {
                 cellProperties.type = "autocomplete";
                 cellProperties.source = [""].concat(removeNull(eventTables.map(function (eventTable, idx) {
@@ -754,11 +919,7 @@ var updateVariables = function () {
     });
     experimentTableData.forEach(function (rowData) {
         var channel = rowData[0];
-        var channelVariables = {};
-        var channelEventIDs = [...new Set(removeNull(rowData.slice(2)))];
-        channelEventIDs.forEach(function (eventID) {
-            channelVariables = Object.assign({}, channelVariables, getEventVariables(eventID));
-        });
+        var channelVariables = getChannelVariables(rowData);
         Object.keys(channelVariables).sort().forEach(function (variableName) {
             if (!Object.keys(globalVariables).includes(variableName)) {
                 var paramType = channelVariables[variableName];
@@ -768,6 +929,15 @@ var updateVariables = function () {
             }
         });
     });
+}
+
+var getChannelVariables = function (experimentTableRowData) {
+    var channelVariables = {};
+    var channelEventIDs = [...new Set(removeNull(experimentTableRowData.slice(2)))];
+    channelEventIDs.forEach(function (eventID) {
+        channelVariables = Object.assign({}, channelVariables, getEventVariables(eventID));
+    });
+    return channelVariables;
 }
 
 // Add any new variables after changes in event tables
